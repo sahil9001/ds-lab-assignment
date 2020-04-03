28759809bcddf3e035c1d25f1b43d1ac4fc3ccda
sahil9001 <sahilsilare@gmail.com>
2020-04-03 17:27:10 +0530

    Changes to be committed: 	new file:   NOTSOIMPORTANT/height_of_binary_tree.cpp 	new file:   NOTSOIMPORTANT/inorder_traversal.cpp 	new file:   NOTSOIMPORTANT/level_order_traversal.cpp 	new file:   NOTSOIMPORTANT/postorder_traversal.cpp

---
+++ NOTSOIMPORTANT/level_order_traversal.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+*/
+
+    void levelOrder(Node * root) {
+        if(root == NULL) return;
+        queue<Node* > q;
+        Node* curr;
+        q.push(root);
+        q.push(NULL);
+        while(q.size()>1){
+            curr =  q.front();
+            q.pop();
+            if(curr == NULL){
+                q.push(NULL);
+               
+            }
+            else{
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+                cout << curr->data << " ";
+            }
+        }
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.levelOrder(root);
+    return 0;
+}
+
